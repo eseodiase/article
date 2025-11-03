4a0cd369cf263fdf4c5fc94e42b6014c1e6c6311
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2025-11-03 14:29:56 +0100

    Update Main.jsx by manually formatting with Prettier.

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,93 +1,146 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
 const Main = () => {
   const shareIconRef = useRef(null);
   const socialIconRef = useRef(null);
-  
+
   const handleShareClick = () => {
     if (shareIconRef.current) {
-      shareIconRef.current.classList.toggle('component__share-button-toggle');
+      shareIconRef.current.classList.toggle("component__share-button-toggle");
     }
-    
+
     if (socialIconRef.current) {
-      socialIconRef.current.classList.toggle('social-icon__toggle');
+      socialIconRef.current.classList.toggle("social-icon__toggle");
     }
   };
-  
+
   return (
     <main className="main">
-    
-    <div className="overlay">
-    
-    <article className="component">
-      <h2 className="component__heading" aria-hidden="true">Component</h2>
-      
-      <picture className="component__picture">
-        <source srcset="drawers.avif" type="image/avif"/>
-        <source srcset="drawers.webp" type="image/webp"/>
-        <img src="drawers.jpg" alt="Drawer Image" loading="eager" className="component__img"/>
-      </picture>
-      
-      <section className="component__article">
-        <h2 className="component__article-heading">
-          Shift the overall look and feel by adding these wonderful
-          touches to furniture in your home
-        </h2>
-        
-        <p className="component__article-text">
-          Ever been in a room and felt like something was missing? Perhaps
-          it felt slightly bare and uninviting. I’ve got some simple tips
-          to help you make any room feel complete.
-        </p>
-        
-        <div className="social-icon">
+      <div className="overlay">
+        <article className="component">
+          <h2 className="component__heading" aria-hidden="true">
+            Component
+          </h2>
+
+          <picture className="component__picture">
+            <source srcset="drawers.avif" type="image/avif" />
+            <source srcset="drawers.webp" type="image/webp" />
+            <img
+              src="drawers.jpg"
+              alt="Drawer Image"
+              loading="eager"
+              className="component__img"
+            />
+          </picture>
+
+          <section className="component__article">
+            <h2 className="component__article-heading">
+              Shift the overall look and feel by adding these wonderful touches
+              to furniture in your home
+            </h2>
+
+            <p className="component__article-text">
+              Ever been in a room and felt like something was missing? Perhaps
+              it felt slightly bare and uninviting. I’ve got some simple tips to
+              help you make any room feel complete.
+            </p>
+
+            <div className="social-icon">
+              <h3 className="social-icon__heading">Share</h3>
+              <a
+                href="#"
+                className="social-icon__link social-icon__link--facebook"
+                aria-label="Share on Facebook"
+              >
+                <img
+                  src="icon-facebook.svg"
+                  alt="Facebook icon"
+                  loading="lazy"
+                />
+              </a>
+              <a
+                href="#"
+                className="social-icon__link social-icon__link--twitter"
+                aria-label="Share on Twitter"
+              >
+                <img src="icon-twitter.svg" alt="Twitter icon" loading="lazy" />
+              </a>
+              <a
+                href="#"
+                className="social-icon__link social-icon__link--pinterest"
+                aria-label="Share on Pinterest"
+              >
+                <img
+                  src="icon-pinterest.svg"
+                  alt="Pinterest icon"
+                  loading="lazy"
+                />
+              </a>
+            </div>
+
+            <div className="component__author-info">
+              <div className="component__author-identity">
+                <img
+                  src="avatar-michelle.jpg"
+                  width="45"
+                  height="45"
+                  alt="Michelle avatar"
+                  loading="lazy"
+                  className="component__author-img"
+                />
+
+                <div className="component__author-more">
+                  <h3 className="component__author-name">Michelle Appleton</h3>
+
+                  <p className="component__date">28 Jun 2020</p>
+                </div>
+              </div>
+
+              {/* Share button */}
+              <button
+                type="button"
+                aria-label="Toggle social media share options"
+                aria-pressed="false"
+                className="component__share-button"
+                onClick={handleShareClick}
+              >
+                <span
+                  className="component__share-icon"
+                  ref={shareIconRef}
+                ></span>
+              </button>
+            </div>
+          </section>
+        </article>
+
+        {/* Social media icons */}
+        <section className="social-icon" ref={socialIconRef}>
           <h3 className="social-icon__heading">Share</h3>
-          <a href="#" className="social-icon__link social-icon__link--facebook" aria-label="Share on Facebook">
-           <img src="icon-facebook.svg" alt="Facebook icon" loading="lazy" />
+          <a
+            href="#"
+            className="social-icon__link social-icon__link--facebook"
+            aria-label="Share on Facebook"
+          >
+            <img src="icon-facebook.svg" alt="Facebook icon" loading="lazy" />
           </a>
-          <a href="#" className="social-icon__link social-icon__link--twitter" aria-label="Share on Twitter">
-          <img src="icon-twitter.svg" alt="Twitter icon" loading="lazy" />
+          <a
+            href="#"
+            className="social-icon__link social-icon__link--twitter"
+            aria-label="Share on Twitter"
+          >
+            <img src="icon-twitter.svg" alt="Twitter icon" loading="lazy" />
           </a>
-          <a href="#" className="social-icon__link social-icon__link--pinterest" aria-label="Share on Pinterest">
-          <img src="icon-pinterest.svg" alt="Pinterest icon" loading="lazy" />
+          <a
+            href="#"
+            className="social-icon__link social-icon__link--pinterest"
+            aria-label="Share on Pinterest"
+          >
+            <img src="icon-pinterest.svg" alt="Pinterest icon" loading="lazy" />
           </a>
-        </div>
-        
-        <div className="component__author-info">
-         <div className="component__author-identity">
-          <img src="avatar-michelle.jpg" width="45" height="45" alt="Michelle avatar" loading="lazy" className="component__author-img"/>
-          
-          <div className="component__author-more">
-            <h3 className="component__author-name">Michelle Appleton</h3>
-            
-            <p className="component__date">28 Jun 2020</p>
-          </div>
-         </div>
-            
-            {/* Share button */}
-            <button type="button" aria-label="Toggle social media share options" aria-pressed="false" className="component__share-button" onClick={handleShareClick}>
-              <span className="component__share-icon" ref={shareIconRef}></span>
-            </button>
-        </div>
-      </section>
-    </article>
-    
-    {/* Social media icons */}
-    <section className="social-icon" ref={socialIconRef}>
-      <h3 className="social-icon__heading">Share</h3>
-      <a href="#" className="social-icon__link social-icon__link--facebook" aria-label="Share on Facebook">
-        <img src="icon-facebook.svg" alt="Facebook icon" loading="lazy"/>
-      </a>
-      <a href="#" className="social-icon__link social-icon__link--twitter" aria-label="Share on Twitter">
-        <img src="icon-twitter.svg" alt="Twitter icon" loading="lazy"/>
-      </a>
-      <a href="#" className="social-icon__link social-icon__link--pinterest" aria-label="Share on Pinterest">
-        <img src="icon-pinterest.svg" alt="Pinterest icon" loading="lazy"/>
-      </a>
-    </section>
-    
-    </div>
-  </main>
+        </section>
+      </div>
+    </main>
   );
-}
+};
 export default Main;
+
